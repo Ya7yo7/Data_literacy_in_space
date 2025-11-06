ad121cd38d5adf2fcae6028663ef8249e6ed0375
Geeniee1 <stockfeltedwind@gmail.com>
2025-11-06 13:39:59 +0100

    Added the results in a docstring and commented a way the csv generator

--- data_cleaning/coordinate_search.py
+++ data_cleaning/coordinate_search.py
@@ -1,3 +1,12 @@
+'''Results:
+Mission,Latitude_center,Longitude_center,Min_latitude,Max_latitude,Westernmost_longitude,Easternmost_longitude
+Spirit,-14.5692,175.4729,-15.0692,-14.0692,174.9729,175.9729
+Opportunity,-1.9483,354.4742,-2.4482999999999997,-1.4483,353.9742,354.9742
+Phoenix,68.22,234.25,67.72,68.72,233.75,234.75
+Curiosity,-4.5895,137.4417,-5.0895,-4.0895,136.9417,137.9417
+InSight,4.502,135.623,4.002,5.002,135.123,136.123
+Perseverance,18.44,77.45,17.94,18.94,76.95,77.95'''
+
 import pandas as pd
 
 # Define Mars landing sites (planetocentric latitude, east longitude)
@@ -36,4 +45,4 @@ df = pd.DataFrame(records)
 print(df)
 
 # Optional: save to CSV for easy copy into ODE
-df.to_csv("mars_landing_boxes_1deg.csv", index=False)
+#df.to_csv("mars_landing_boxes_1deg.csv", index=False)
